bb90e76101db66ace456abdf5c26a7561433345d
phamngocduy <phmngcduy@gmail.com>
2024-05-04 19:53:06 -0400

    Upgrade to Promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,40 +1,71 @@
 const router = require('express').Router()
 let books = require('./booksdb.js')
-let isValid = require('./auth_users.js').isValid
-let users = require('./auth_users.js').users
 
 
 // Get the book list available in the shop
 router.get('/', (req, res) => {
-    res.send(books)
+    new Promise((resolve, reject) => {
+        try {
+            resolve(require('./booksdb.js'))
+        } catch (error) {
+            console.log(error)
+            reject(error.message)
+        }
+    }).then(books => res.send(books),
+            error => res.status(208).send(error))
 })
   
 // Get book details based on ISBN
 router.get('/isbn/:isbn', (req, res) => {
     const isbn = req.params.isbn
-    res.send(books[isbn])
+    new Promise((resolve, reject) => {
+        try {
+            let books = require('./booksdb.js')
+            resolve(books[isbn])
+        } catch (error) {
+            console.log(error)
+            reject(error.message)
+        }
+    }).then(books => res.send(books),
+            error => res.status(208).send(error))
 })
 
 // Get book details based on author
 router.get('/author/:author', (req, res) => {
     const author = req.params.author
-    filteredBooks = []
-    Object.keys(books).forEach(key => {
-        if (books[key].author === author)
-            filteredBooks.push(books[key])
-    })
-    res.send(filteredBooks)
+    new Promise((resolve, reject) => {
+        try {
+            filteredBooks = []
+            Object.keys(books).forEach(key => {
+                if (books[key].author === author)
+                    filteredBooks.push(books[key])
+            })
+            resolve(filteredBooks)
+        } catch (error) {
+            console.log(error)
+            reject(error.message)
+        }
+    }).then(books => res.send(books),
+            error => res.status(208).send(error))
 })
   
 // Get all books based on title
 router.get('/title/:title', (req, res) => {
     const title = req.params.title
-    filteredBooks = []
-    Object.keys(books).map(key => {
-        if (books[key].title === title)
-            filteredBooks.push(books[key])
-    })
-    res.send(filteredBooks)
+    new Promise((resolve, reject) => {
+        try {
+            filteredBooks = []
+            Object.keys(books).map(key => {
+                if (books[key].title === title)
+                    filteredBooks.push(books[key])
+            })
+            resolve(filteredBooks)
+        } catch (error) {
+            console.log(error)
+            reject(error.message)
+        }
+    }).then(books => res.send(books),
+            error => res.status(208).send(error))
 })
 
 //  Get book review
