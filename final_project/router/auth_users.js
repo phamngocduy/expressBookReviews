71d955b427f744d435263617900de91b6e3b83f2
phamngocduy <phmngcduy@gmail.com>
2024-05-04 19:27:11 -0400

    Finish all functions

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -52,7 +52,7 @@ router.post('/login', (req, res) => {
         req.session.authorization = {
             accessToken, username
         }
-        return res.status(200).send('User successfully logged in')
+        return res.status(200).json({message: 'User successfully logged in'})
     } else
         return res.status(208).json({message: 'Invalid Login. Check username and password'})
 })
@@ -62,23 +62,30 @@ router.get('/login', (req, res) => {
 })
 
 // Add a book review
-router.get('/auth/review/:isbn', (req, res) => {
-    const isbn = req.query.isbn
-    const content = req.query.content
+router.put('/auth/review/:isbn', (req, res) => {
+    const isbn = req.params.isbn
+    const review = req.body.review
     const username = req.session.authorization.username
 
     selectedBook = books[isbn]
-    console.log(selectedBook)
     if (selectedBook != undefined) {
-        selectedBook.reviews[username] = content
-        return res.status(200).json({message: 'Review added successfully'})
+        selectedBook.reviews[username] = review
+        return res.status(200).json({message: `The review for the book with ISBN ${isbn} has been added/updated.`})
     }
     return res.status(208).json({message: 'Book not found'})
 })
 
 // Delete a book review
-router.get('/review/:isbn', (req, res) => {
-    const isbn = req.query.isbn
+router.delete('/auth/review/:isbn', (req, res) => {
+    const isbn = req.params.isbn
+    const username = req.session.authorization.username
+
+    selectedBook = books[isbn]
+    if (selectedBook != undefined) {
+        delete selectedBook.reviews[username]
+        return res.status(200).json({message: `Reviews for the ISBN ${isbn} posted by the user ${username} deleted.`})
+    }
+    return res.status(208).json({message: 'Book not found'})
 })
 
 module.exports.router = router
